89ee7b53dbc63e437133615f62596008d952310d
Justin Kuo <justin831201@gmail.com>
2022-10-03 04:11:50 +0800

    echo more hints at entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,9 @@ set -e
 case "$@" in
 
 run_trading_service)
+    echo "Wait for Infra Services..."
     sleep 10s
+    echo "Start service..."
     exec /srv/${PROJECT_NAME}/bin/${APP_NAME} -c /srv/${PROJECT_NAME}/config/config.yaml
     ;;
 
