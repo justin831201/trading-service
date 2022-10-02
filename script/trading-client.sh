1504a6a46e3c66fe692a4d90162783b3d0e0b133
Justin Kuo <justin831201@gmail.com>
2022-10-03 03:21:40 +0800

    add trading-client shell script

---
+++ script/trading-client.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Mock 2000 orders
+for _ in $(seq 1 10)
+do
+    user_id=$(uuidgen)
+    order_type=$(($RANDOM % 2))
+    price_type=$(($RANDOM % 2))
+    quantity=$(($RANDOM % 99 + 1))
+
+    if [ $price_type -eq 0 ]
+    then
+        curl -X POST -d "{\"user_id\": \"$user_id\", \"order_type\": $order_type, \"quantity\": $quantity, \"price_type\": $price_type}" http://localhost:8080/order
+    else
+        price=$(($RANDOM % 10 + 95))
+        curl -X POST -d "{\"user_id\": \"$user_id\", \"order_type\": $order_type, \"quantity\": $quantity, \"price_type\": $price_type, \"price\": $price}" http://localhost:8080/order
+    fi
+    echo
+done
